7e69db3bc84d6e55ce833e13eea8e81d023ee40b
vanlach <36769904+vanlach@users.noreply.github.com>
2024-06-22 17:06:59 -0500

    Attack after waking up from sleep and changing maximum turns asleep to 6 like in gen 2
    https://github.com/pret/pokered/wiki/Attack-after-waking-up-from-sleep

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -315,7 +315,7 @@ MainInBattleLoop:
 	and a
 	ret nz ; return if pokedoll was used to escape from battle
 	ld a, [wBattleMonStatus]
-	and (1 << FRZ) | SLP_MASK
+	and (1 << FRZ)
 	jr nz, .selectEnemyMove ; if so, jump
 	ld a, [wPlayerBattleStatus1]
 	and (1 << STORING_ENERGY) | (1 << USING_TRAPPING_MOVE) ; check player is using Bide or using a multi-turn attack like wrap
@@ -3357,6 +3357,7 @@ CheckPlayerStatusConditions:
 .WakeUp
 	ld hl, WokeUpText
 	call PrintText
+	jr z, .FrozenCheck
 .sleepDone
 	xor a
 	ld [wPlayerUsedMove], a
@@ -5835,6 +5836,7 @@ CheckEnemyStatusConditions:
 .wokeUp
 	ld hl, WokeUpText
 	call PrintText
+	jr z, .fheckIfFrozen
 .sleepDone
 	xor a
 	ld [wEnemyUsedMove], a
